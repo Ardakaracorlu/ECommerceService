90cd9a9c72dc0045b5c1900c6b93ed6c6d059b7a
karacorlu98@gmail.com <karacorlu98@gmail.com>
2024-10-07 13:30:58 +0300

    --

--- Order.Api/Controllers/OrderController.cs
+++ Order.Api/Controllers/OrderController.cs
@@ -26,31 +26,33 @@ namespace Order.Api.Controllers
         [HttpPost("CreateOrder")]
         public async Task<IActionResult> CreateOrder(CreateOrderRequest createOrderRequest)
         {
-            OrderInfo orderInfo = new OrderInfo
+            for (int i = 0; i < 5; i++)
             {
-                ProductId = createOrderRequest.ProductId,
-                Quantity = createOrderRequest.Quantity,
-                CustomerName = createOrderRequest.CustomerName,
-                CustomerSurname = createOrderRequest.CustomerSurname,
-                Phone = createOrderRequest.Phone,
-                Adress = createOrderRequest.Adress,
-                Email = createOrderRequest.Email,
-                Status = OrderStatus.OrderReceived,
-                Description = "Sipariş Alındı"   
-            };
+                OrderInfo orderInfo = new OrderInfo
+                {
+                    ProductId = createOrderRequest.ProductId,
+                    Quantity = createOrderRequest.Quantity,
+                    CustomerName = createOrderRequest.CustomerName,
+                    CustomerSurname = createOrderRequest.CustomerSurname,
+                    Phone = createOrderRequest.Phone,
+                    Adress = createOrderRequest.Adress,
+                    Email = createOrderRequest.Email,
+                    Status = OrderStatus.OrderReceived,
+                    Description = "Sipariş Alındı"
+                };
 
-            await _orderDbContext.AddAsync(orderInfo);
-            await _orderDbContext.SaveChangesAsync();
+                await _orderDbContext.AddAsync(orderInfo);
+                await _orderDbContext.SaveChangesAsync();
 
-            StockQueueRequest stockQueueRequest = new StockQueueRequest
-            {
-                OrderId = orderInfo.Id,
-                ProductId = createOrderRequest.ProductId,
-                Quantity = createOrderRequest.Quantity,
-            };
+                StockQueueRequest stockQueueRequest = new StockQueueRequest
+                {
+                    OrderId = orderInfo.Id,
+                    ProductId = createOrderRequest.ProductId,
+                    Quantity = createOrderRequest.Quantity,
+                };
 
-            _queueOperation.PublishMessage(stockQueueRequest, "stock_queue", "stock.direct", "stock_key", 0); // StockQueue
-
+                _queueOperation.PublishMessage(stockQueueRequest, "stock_queue", "stock.direct", "stock_key", 0); // StockQueue
+            }
             return Ok("Siparişiniz Başarıyla Alındı");
         }
     }
